1353c441e4d1f9a8b3f4746bdb57a8f5a94685d2
Mehul <119481480+Mehul6112@users.noreply.github.com>
2023-02-20 10:53:07 +0530

    version 1.5

--- Maths_5(solving homogen eqn using Gauss-Jordan).py
+++ Maths_5(solving homogen eqn using Gauss-Jordan).py
@@ -1,34 +1,20 @@
 import numpy as np
-from sympy import Matrix
-from sympy import *
 
-#5.Solve a System of Homogeneous Equations Using The Gauss Jordan method:
-def gauss_jordan(A):
-    n = A.shape[0]
-    for i in range(n):
-        # Find the pivot element
-        max_element = np.abs(A[i, i])
-        max_row = i
-        for k in range(i + 1, n):
-            if np.abs(A[k, i]) > max_element:
-                max_element = np.abs(A[k, i])
-                max_row = k
-        # Swap the current row with the pivot row
-        if max_row != i:
-            A[[i, max_row]] = A[[max_row, i]]
-        # Normalize the current row
-        A[i, :] = A[i, :] / A[i, i]
-        # Eliminate all elements above and below the pivot element
-        for k in range(n):
-            if k != i:
-                c = A[k, i]
-                A[k, :] = A[k, :] - c * A[i, :]
-    return A
+print("-----GAUSS-JORDAN METHOD-----")
 
-print("\n GAUSS JORDAN OF THE MATRIX\n")
+A = np.matrix(input("Write elements separated by spaces and separate rows with semi-colon: \n"))
+print("Matrix A is: \n", A)
 
-# Example Homogeneous Equation:
-A = np.array([[1, 2, 3], [4, 5, 2], [5, 7, 0]])
-print("Given matrix: ", A)
-A = gauss_jordan(A)
-print("\n Reduced row-echelon form of The Given Matrix is:\n",A)
+B = np.matrix(input("\n Write elements separated by spaces and separate rows with semi-colon: \n"))
+print("Matrix B is: \n", B) 
+
+# Compute the inverse of A using numpy.linalg.inv()
+A_inv = np.linalg.inv(A)
+print("/n Inverse of Matrix A is: \n {}".format(A_inv))
+
+# Compute the solution vector x using the Gauss-Jordan method
+x = np.dot(A_inv, B)
+
+# Print the solution vector
+print("\n Solution vector: \n")
+print(x)
