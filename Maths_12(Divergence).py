0bd5393cdb4933ddec6a60b62d0336d18c332d3f
Mehul <119481480+Mehul6112@users.noreply.github.com>
2023-02-21 18:46:20 +0530

    version 1.6
    more comprehensible

--- Maths_12(Divergence).py
+++ Maths_12(Divergence).py
@@ -1,16 +1,23 @@
 import numpy as np
+import sympy
 #12.>Compute Divergence of a vector field
 
 print("\n DIVERGENCE  OF THE MATRIX\n")
-#Creating a Matrix
-
-matrix= np.array([6, 4, 9, 13, 22, 69], dtype=float)
-
-#Finding Divergence of a Matrix
 
 def divergence(F):
-    return np.ufunc.reduce(np.add,np.gradient(F))
+    # Computing the partial derivatives of the vector field components
+    dPdx = sympy.diff(F[0], x)
+    dQdy = sympy.diff(F[1], y)
+    dRdz = sympy.diff(F[2], z)
+    
+    #Computing the divergence of the vector field
+    divF = dPdx + dQdy + dRdz
+    return divF
+x, y, z = sympy.symbols('x y z')
+F = sympy.Matrix([(x**2)*z, (-2)*(y**3)*(z**2), x*(y**2)*z])
 
-print("Input  :\n ", matrix)
+# Computing the divergence of F
+divF = divergence(F)
 
-print("Divergence :\n ",divergence(matrix))
+print("The divergence of the vector field {} is: \n".format(F))
+print(divF.simplify())
