6454bdec5bd6da9f775946ef00cba328955c4387
Mehul <119481480+Mehul6112@users.noreply.github.com>
2023-02-21 12:47:44 +0530

    version 1.5

--- Maths_5(solving homogen eqn using Gauss-Jordan).py
+++ Maths_5(solving homogen eqn using Gauss-Jordan).py
@@ -1,20 +1,22 @@
 import numpy as np
+print("\n-----GAUSS-JORDAN METHOD-----\n")
 
-print("-----GAUSS-JORDAN METHOD-----")
+A = np.matrix(input("Write elements separated by spaces and separate rows with semi-colon: "))
+print("Matrix A is \n: ", A, "\n")
+#Example_A = 1 1 1;2 -3 4;3 4 5
 
-A = np.matrix(input("Write elements separated by spaces and separate rows with semi-colon: \n"))
-print("Matrix A is: \n", A)
+B = np.matrix(input("\n Write elements separated by semi-colon: "))
+print("Matrix B is: \n", B, "\n") 
+#Example_B = 9;13;40
 
-B = np.matrix(input("\n Write elements separated by spaces and separate rows with semi-colon: \n"))
-print("Matrix B is: \n", B) 
-
-# Compute the inverse of A using numpy.linalg.inv()
+#Computing the inverse of A using numpy.linalg.inv()
 A_inv = np.linalg.inv(A)
-print("/n Inverse of Matrix A is: \n {}".format(A_inv))
+print("\n Inverse of Matrix A is: \n {}".format(A_inv))
 
-# Compute the solution vector x using the Gauss-Jordan method
+#Computing the solution vector x using the Gauss-Jordan method
 x = np.dot(A_inv, B)
 
-# Print the solution vector
+# Printing the solution vector
 print("\n Solution vector: \n")
 print(x)
+#Solution_X = 1;3;5
