0b5a0ddd9eabeed781a3e89f75647b52f1c5645f
Mehul <119481480+Mehul6112@users.noreply.github.com>
2023-02-21 18:50:53 +0530

    version 1.7

--- Maths_13(Curl).py
+++ Maths_13(Curl).py
@@ -1,24 +1,17 @@
-import numpy as np
-from sympy import Matrix
-from sympy import *
+import sympy as sp
+print("\n CURL OF THE MATRIX\n")
 
-#13.>Compute Curl of a vector field.
- 
-print("\n CURL OF THE VECTOR FIELD\n")
+# Define the symbols
+x, y, z = sp.symbols('x y z')
 
-# Define the vector field as a NumPy array
-# For example, the vector field F = (y^2*z, x*z^2, x*y^2)
-def F(x, y, z):
-    return np.array([y**2*z, x*z**2, x*y**2])
+# Define the vector field
+A = sp.Matrix([x*z**3, -2*x**2*y*z, 2*y*z**4])
 
-# Define the curl of the vector field as a function of x, y, z
-# The curl is given by the cross product of the gradient with the vector field
-def curl_F(x, y, z):
-    curl_x = (F(x + 0.01, y, z)[2] - F(x - 0.01, y, z)[2] - F(x, y + 0.01, z)[1] + F(x, y - 0.01, z)[1]) / 0.02
-    curl_y = (F(x, y + 0.01, z)[0] - F(x, y - 0.01, z)[0] - F(x, y, z + 0.01)[2] + F(x, y, z - 0.01)[2]) / 0.02
-    curl_z = (F(x, y, z + 0.01)[1] - F(x, y, z - 0.01)[1] - F(x + 0.01, y, z)[0] + F(x - 0.01, y, z)[0]) / 0.02
-    return np.array([curl_x, curl_y, curl_z])
+# Compute the curl of the vector field
+curl_A = sp.Matrix([sp.diff(A[2], y) - sp.diff(A[1], z),
+                    sp.diff(A[0], z) - sp.diff(A[2], x),
+                    sp.diff(A[1], x) - sp.diff(A[0], y)])
 
-# Test the function with an example
-x, y, z = 1, 2, 3
-print("The curl of F at ({},{},{}) is: {}".format(x, y, z, curl_F(x, y, z)))
+curl_A.simplify()
+print("The curl of the vector field {} is: \n".format(A))
+print(curl_A)
